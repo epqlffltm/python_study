457cd4f9ecdc7001484d3b57172e83f488d54570
kjh55 <kjh55@DESKTOP-M8NJ67D>
2023-01-11 10:22:12 +0900

    초기 커밋.

---
+++ python_study.py
@@ -0,0 +1,213 @@
+a,b=('python','lifi')
+
+print(a)
+print(b)
+
+n=[1,2,3,4,5]
+m=n
+l=n[:]
+n[1]=7
+l[1]='a'
+
+print(n)
+print(m)
+print(l)
+
+#a=[1,2,3]
+#b=a[:]
+
+#a[1]=4
+#a[2]="a"
+
+#print(a,b)
+
+
+#a=[1,2,3,4]
+
+#while a:
+#    a.pop()
+#    print(a)
+
+#a="hi"
+
+#if a:
+#    print(a)
+
+#a=True
+#print(type(a))
+#print(a)
+
+#s1=set([1,2,3,4,5,"a","b","c"])
+#s1.add(0)
+#s1.update([6,7,8,9])
+#print(s1)
+
+
+#s1=set("hallow")
+#print(s1)
+
+#s1= set([1,2,3])
+#s2={1,2,3}
+#print(type(s1))
+
+#i=[1,2,2,3,3]
+#newlist=list(set(i))
+#print(newlist)
+
+#a={1:'hong',2:'gil', 3:'dong'}
+
+
+#print(a.get(4,'no'))
+
+#for k,v in a.items():
+#    print("key:"+str(k))
+#    print("values:"+v)
+
+#for k in a.keys():
+#    print(k)
+
+#print(a.keys())
+
+#print(a.values())
+
+#print(a.items())
+#print(a)
+
+#a={1:'hommer',1:'maji'}
+
+#print(a)
+
+#a={1:'a'}
+#a['name']="dlraud"
+#print(a)
+
+#dic={'name':'eric','age':15}
+#print(dic['name'])
+
+#t1=(1,2,3,4,'a')
+#t2=(5)
+#t1=t1*3
+
+#print(t1*t2)
+
+#t1=(1,2,'a','b')
+
+
+#print(t1[0:5:2])
+
+#a=[1,2,3,4,5]
+
+#a.extend([6,7,8,9])
+
+#print(a)
+
+#a=[1,3,5,7,9,1,1]
+
+#print(a.count(1))
+
+#print(a)
+
+
+
+#a="happy"
+
+#print(a.upper())
+
+#a="happy
+#"
+
+#print(a.count("x"))
+
+
+#pi="%0.4f" %3.14152359730294
+
+#print(pi)
+
+#name="hong-gildong"
+#a=f"i am {name}"
+#print(a)
+#
+#
+#a="Blood for the Blood god! {name} Skull for the Skull Throne!".format(name="hong-gildong")
+
+#print(a)
+
+
+#number=5
+#day="5"
+#a="i eat %d apple, so i was sick for %s day" %(number,day)
+
+#print(a)
+
+#a="Blood for the Blood god! %d Skull for the Skull Throne!" %3
+#b= "Blood for the Blood god! %d Skull for the Skull Throne! %d" %(10, 5)
+#print(a[::])
+#print(b)
+
+#num=5;
+#day=3;
+
+#a="i,m %d eat %dday"%(num,day)
+
+#print(a)
+
+#a="0123456789"
+
+#print(a[0:5:3])
+
+#a="coll of duty"
+
+#print(a[0:4:2])
+
+#a="hallow"
+#b="python"
+
+#print(a+"\n"+b*5)
+
+#a="hallow"
+
+#b="wared"
+
+#c="python"
+
+#print("%s\n%s"%(a,b))
+#print(a + "\n" + b)
+
+#print("abcd\tefg\n")
+
+#a='life is too need'
+#b="fun"
+
+#print(a,b)
+
+#print(a*10)
+
+#print(b,)
+
+#print("python'hallow")
+
+#print("ab|cd")
+
+#print("hallow\nwated!\thallow\t\npython")
+
+#a=5
+#b=15
+#print(a/b)
+#print(a//b)
+
+#a=1
+#b=5.05
+#c="1"
+#print(type(a))
+#print(type(b))
+#print(type(c))
+
+#a=3;
+#a=a+1;
+#print(a);
+
+#a=1;
+#b=2;
+#print(a+b);
+
+#
